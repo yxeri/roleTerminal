3eabea4a0a568b742fc163df31cb94e45f675817
Aleksandar Jankovic <aleksonline@icloud.com>
2015-09-12 17:27:14 +0200

    Text change

--- routes/error.js
+++ routes/error.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 function handle() {
   router.get('/', function(req, res) {
-    res.status(404).render('error', { title : 'Organica Oracle v3.2 - Error' });
+    res.status(404).render('error', { title : 'Organica Oracle v4.0 - Error' });
   });
   return router;
 }

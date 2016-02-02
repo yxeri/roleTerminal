ea5bafcbb1946d7c3e687ca22b071e6343c2a46b
Aleksandar Jankovic <aleksonline@icloud.com>
2016-02-02 09:54:38 +0100

    More documentation for config. Less default values specific for Blodsband LARP

--- config/appConfig.js
+++ config/appConfig.js
@@ -13,10 +13,32 @@ config.gameLocation = {
   lat: process.env.LATITUDE || '59.751429',
   lon: process.env.LONGITUDE || '15.198645',
 };
+
+// Amount of messages retrieved with the history command
 config.historyLines = process.env.MAXHISTORY || 80;
+
+// Amount of messages sent at a time to client
 config.chunkLength = process.env.MAXCHUNK || 10;
+
+// Does the user have to be verified before being able to login?
 config.userVerify = process.env.REQUIREVERIFY || false;
-config.title = process.env.TITLE || 'Organica Oracle v4.0';
+
+// Title of the site
+config.title = process.env.TITLE || 'roleHaven';
+
+/**
+ * Default mode for command input.
+ * Valid options are: cmd  chat
+ */
 config.defaultMode = process.env.DEFAULTMODE || dbDefaults.modes.command;
+
+//
+/**
+ * Default language for clients connecting.
+ * Default language is English.
+ * Don't set this var if you want English to be the default langauge.
+ * Valid options:
+ */
+config.defaultLanguage = process.env.DEFAULTLANGUAGE || '';
 
 module.exports = config;

9c21a2d5d69dd5ceb4c1f3950ca2baeee24a3b57
Aleksandar Jankovic <aleksonline@icloud.com>
2016-06-19 16:43:38 +0200

    Hackstation test command

---
+++ private/scripts/cmdsBbr.js
@@ -0,0 +1,26 @@
+/** @module */
+
+const textTools = require('./textTools');
+const messenger = require('./messenger');
+
+const commands = {};
+
+commands.hackstation = {
+  func: () => {
+    const text = textTools.createMixedArray({
+      amount: 24,
+      length: 28,
+      upperCase: false,
+      codeMode: true,
+      requiredStrings: ['BANANA', 'APPLE', 'GRAPE', 'magnet'],
+    });
+
+    messenger.queueMessage({ text });
+  },
+  accessLevel: 1,
+  visibility: 1,
+  category: 'basic',
+  commandName: 'hackstation',
+};
+
+module.exports = commands;

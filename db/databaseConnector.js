bcb356468511684176cbf377c2fac69dde3be3f9
Aleksandar Jankovic <aleksonline@icloud.com>
2016-08-20 17:25:57 +0200

    Moved mission db calls to separate file

--- db/databaseConnector.js
+++ db/databaseConnector.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const ObjectId = require('mongodb').ObjectID;
 const mongoose = require('mongoose');
 const appConfig = require('./../config/defaults/config').app;
 const logger = require('./../utils/logger');
@@ -42,20 +41,6 @@ const weatherSchema = new mongoose.Schema({
   windDirection: Number,
   thunderRisk: Number,
 }, { collection: 'weather' });
-const missionSchema = new mongoose.Schema({
-  timeCreated: Date,
-  completed: { type: Boolean, default: false },
-  reward: String,
-  title: String,
-  description: String,
-  requirement: String,
-  creator: String,
-  agent: String,
-  applicationRequired: { type: Boolean, default: false },
-  missionType: String,
-  visibility: Number,
-  accessLevel: Number,
-}, { collection: 'missions' });
 const invitationListSchema = new mongoose.Schema({
   userName: { type: String, unique: true, index: true },
   invitations: [{
@@ -77,26 +62,9 @@ const gamePasswordSchema = new mongoose.Schema({
 const ScheduledEvent = mongoose.model('ScheduledEvent', scheduledEventSchema);
 const Team = mongoose.model('Team', teamSchema);
 const Weather = mongoose.model('Weather', weatherSchema);
-const Mission = mongoose.model('Mission', missionSchema);
 const InvitationList = mongoose.model('InvitationList', invitationListSchema);
 const GameUser = mongoose.model('GameUser', gameUserSchema);
 const GamePassword = mongoose.model('GamePassword', gamePasswordSchema);
-
-function updateMissionValue(missionId, update, callback) {
-  const query = { _id: missionId };
-
-  Mission.findOneAndUpdate(query, update).lean().exec((err, mission) => {
-    if (err) {
-      logger.sendErrorMsg({
-        code: logger.ErrorCodes.db,
-        text: ['Failed to update mission'],
-        err,
-      });
-    }
-
-    callback(err, mission);
-  });
-}
 
 function saveObject(object, objectName, callback) {
   object.save((saveErr, savedObj) => {
@@ -142,58 +110,6 @@ function verifyAllObjects(query, object, objectName, callback) {
     }
 
     callback(err, verified);
-  });
-}
-
-function createMission(sentMission, callback) {
-  const newMission = new Mission(sentMission);
-
-  saveObject(newMission, 'mission', callback);
-}
-
-function updateMissionCompleted(missionIdString, value, callback) {
-  const update = { completed: value };
-
-  updateMissionValue(new ObjectId(missionIdString), update, callback);
-}
-
-function updateMissionReward(missionIdString, value, callback) {
-  const update = { reward: value };
-
-  updateMissionValue(new ObjectId(missionIdString), update, callback);
-}
-
-function getActiveMissions(callback) {
-  const query = { completed: false };
-  const filter = { _id: 0 };
-
-  Mission.find(query, filter).lean().exec((err, missions) => {
-    if (err) {
-      logger.sendErrorMsg({
-        code: logger.ErrorCodes.db,
-        text: ['Failed to get active missions'],
-        err,
-      });
-    }
-
-    callback(err, missions);
-  });
-}
-
-function getAllMissions(callback) {
-  const query = { };
-  const filter = { _id: 0 };
-
-  Mission.find(query, filter).lean().exec((err, missions) => {
-    if (err) {
-      logger.sendErrorMsg({
-        code: logger.ErrorCodes.db,
-        text: ['Failed to get all missions'],
-        err,
-      });
-    }
-
-    callback(err, missions);
   });
 }
 
@@ -528,11 +444,6 @@ exports.createTeam = createTeam;
 exports.getTeam = getTeam;
 exports.createWeather = createWeather;
 exports.getWeather = getWeather;
-exports.createMission = createMission;
-exports.getActiveMissions = getActiveMissions;
-exports.getAllMissions = getAllMissions;
-exports.updateMissionCompleted = updateMissionCompleted;
-exports.updateMissionReward = updateMissionReward;
 exports.addInvitationToList = addInvitationToList;
 exports.removeInvitationFromList = removeInvitationFromList;
 exports.getInvitations = getInvitations;

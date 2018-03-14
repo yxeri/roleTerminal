d5ea216e4db883cba27ef1aa85deaeb279d5231e
Aleksandar Jankovic <aleksonline@icloud.com>
2018-03-14 11:48:18 +0100

    Updated showfloor.

--- private/scripts/showfloor.js
+++ private/scripts/showfloor.js
@@ -1,6 +1,5 @@
 const socketManager = require('../library/SocketManager');
 const elementCreator = require('../library/ElementCreator');
-const eventCentral = require('../library/EventCentral');
 const viewTools = require('../library/ViewTools');
 
 const info = document.getElementById('info');
@@ -11,28 +10,28 @@ let hackers = [];
 
 const infoContainer = elementCreator.createContainer({});
 infoContainer.appendChild(elementCreator.createParagraph({
-  text: 'roleHaven är ett initiativ för att skapa en universal lajv-app. ' +
-  'roleHaven är en samling av tillgängliga verktyg och spel för både arrangörer och deltagare.',
+  text: 'Rolehaven is an initiative to create a universal larp app. ' +
+  'Rolehaven is a collection of tools and games targeted at both organisers and participants.',
 }));
 infoContainer.appendChild(elementCreator.createParagraph({
-  text: 'Ni kan testa appen på datorerna här eller genom att gå till' +
-  ' bbrterminal.thethirdgift.com (Blodsband) eller tales.thethirdgift.com (Tales from the Sprawl)',
+  text: 'You can try out the app on the computers here or visit' +
+  ' bbrterminal.thethirdgift.com (Blodsband Reloaded. Mad Maxish) or tales.thethirdgift.com (Tales from the Sprawl. Cyberpunk, Shadowrun).',
 }));
 infoContainer.appendChild(elementCreator.createParagraph({
-  text: 'Vill ni veta mer? Besök på www.facebook.com/thethirdgiftgames/ eller www.patreon.com/yxeri. Ni kan kontakta utvecklaren, Aleksandar Jankovic, via aleks@thethirdgift.com.',
+  text: 'Want to know more? Visit www.facebook.com/thethirdgiftgames/ or www.patreon.com/yxeri. You can get in touch with the developer, Aleksandar Jankovic, at aleks@thethirdgift.com.',
 }));
 info.appendChild(infoContainer);
 
 const wreckingContainer = elementCreator.createContainer({ classes: ['wrecking'] });
 wreckingContainer.appendChild(elementCreator.createParagraph({
-  text: 'Blodsband Reloaded hade ett spelmoment som kallades "wrecking." ' +
-  'Spelare hade då som mål att hacka via appen eller fånga stationer (vilket är lådan + parabolen som ni ser här bredvid, som skapats av arrangörerna av Blodsband) för att samla poäng. ' +
-  'Under Prolog så kommer ni ha chans att testa både att fysiskt fånga stationen eller hacka den! En notis kommer att skrivas ut nedan när wreckingen är igång.',
+  text: 'Blodsband Reloaded had a gameplay moment called "wrecking." ' +
+  'The players were tasked with conquering and holding physical stations (created by the Blodsband organisers) in the game area. Those playing hackers would use the app to hack these stations to help/hinder those who held them. ' +
+  'You can try it out yourself on the station (metal box) here when a wrecking round is active. A notice will be printed out here when a round is active.',
 }));
 wrecking.appendChild(wreckingContainer);
 
 const activeContainer = elementCreator.createContainer({});
-activeContainer.appendChild(elementCreator.createParagraph({ classes: ['inactive'], text: 'Wrecking är inte aktiv.' }));
+activeContainer.appendChild(elementCreator.createParagraph({ classes: ['inactive'], text: 'Wrecking is inactive.' }));
 
 window.addEventListener('click', () => {
   viewTools.goFullScreen();
@@ -43,15 +42,15 @@ function buildStats(data) {
   const element = elementCreator.createContainer({});
 
   if (!round.isActive) {
-    element.appendChild(elementCreator.createParagraph({ classes: ['inactive'], text: 'Wrecking är inte igång.' }));
+    element.appendChild(elementCreator.createParagraph({ classes: ['inactive'], text: 'Wrecking is inactive.' }));
     hackers = [];
     hacker.innerHTML = '';
     hacker.appendChild(elementCreator.createContainer({}));
   } else {
     element.appendChild(elementCreator.createParagraph({
       classes: ['active'],
-      text: 'Wrecking är igång! Ni kan delta som hacker genom appen eller gå till stationen (gråa lådan + parabol) för att fånga den. ' +
-      'I appen: Gå till Home (första vyn. Den kan nås genom att klicka på toppraden). Klicka på PROGRAMS, hackLantern och följ sedan instruktionerna!',
+      text: 'Wrecking is active! You can participate through the app or by going to the station (metal box). ' +
+      'In the app: Go to the home view. You can reach it by clicking on the top, if not already there. Click on PROGRAMS, hackLantern and follow the instructions.',
     }));
     hackers = [];
     hacker.innerHTML = '';

56597342034438aefd557f92d0a2c1e7daf4483b
Aleksandar Jankovic <aleksonline@icloud.com>
2018-06-01 20:24:26 +0200

    Alias composer.

---
+++ private/library/data/AliasComposer.js
@@ -0,0 +1,60 @@
+const DataComposer = require('./BaseComposer');
+
+const dataHandler = require('./DataHandler');
+const eventCentral = require('../EventCentral');
+const storageManager = require('../StorageManager');
+
+class AliasComposer extends DataComposer {
+  constructor() {
+    super({
+      handler: dataHandler.aliases,
+      completionEvent: eventCentral.Events.COMPLETE_ALIAS,
+      dependencies: [
+        dataHandler.aliases,
+        dataHandler.users,
+        dataHandler.teams,
+      ],
+    });
+  }
+
+  getCurrentUserAliases() {
+    const userId = storageManager.getUserId();
+
+    if (!userId) { return []; }
+
+    const aliases = this.handler.getObjects({
+      filter: {
+        orCheck: true,
+        rules: [
+          { paramName: 'ownerId', paramValue: storageManager.getUserId() },
+        ],
+      },
+    });
+
+    return aliases;
+  }
+
+  createAlias({
+    aliasName,
+    callback,
+  }) {
+    this.handler.createObject({
+      callback,
+      params: { alias: { aliasName } },
+    });
+  }
+
+  getAliasName({ aliasId }) {
+    const alias = this.handler.getObject({ objectId: aliasId });
+
+    if (!alias) {
+      return '';
+    }
+
+    return alias.aliasName;
+  }
+}
+
+const aliasComposer = new AliasComposer();
+
+module.exports = aliasComposer;

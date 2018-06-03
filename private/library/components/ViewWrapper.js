6a057183bf7085d3a5b4462da95ed6194350e6ba
Aleksandar Jankovic <aleksonline@icloud.com>
2018-06-03 17:17:37 +0200

    View switch feature added

--- private/library/components/ViewWrapper.js
+++ private/library/components/ViewWrapper.js
@@ -6,6 +6,7 @@ class ViewWrapper extends BaseView {
   constructor({
     topView,
     columns,
+    title,
     classes = [],
   }) {
     super({
@@ -13,47 +14,93 @@ class ViewWrapper extends BaseView {
       classes: classes.concat(['viewWrapper']),
     });
 
-    if (topView) {
-      topView.addToView({ element: this.element });
+    this.columnAmount = columns.length;
+    this.topView = topView;
+    this.columns = columns;
+    this.columnElements = [];
+    this.title = title;
+  }
+
+  attachColumns() {
+    const fragment = document.createDocumentFragment();
+
+    this.columnElements.forEach(columnElement => columnElement.remove());
+    this.columnElements = [];
+
+    this.columns.forEach((column) => {
+      const columnElement = this.createColumn({ column });
+
+      this.columnElements.push(columnElement);
+      fragment.appendChild(columnElement);
+    });
+
+    this.element.appendChild(fragment);
+  }
+
+  addToView({ element }) {
+    if (this.topView) {
+      this.topView.addToView({ element: this.element });
     }
 
-    const columnAmount = columns.length;
+    this.attachColumns();
 
-    columns.forEach(({ components, classes: columnClasses = [] }) => {
-      const containerClasses = ['column'].concat(columnClasses);
+    super.addToView({ element });
+  }
 
-      if (columnAmount === 2) {
-        containerClasses.push('oneOfTwo');
-      } else if (columnAmount === 3) {
-        containerClasses.push('oneOfThree');
-      } else {
-        containerClasses.push('one');
+  createColumn({ column }) {
+    const { components, classes: columnClasses = [] } = column;
+
+    const containerClasses = ['column'].concat(columnClasses);
+
+    if (this.columnAmount === 2) {
+      containerClasses.push('oneOfTwo');
+    } else if (this.columnAmount === 3) {
+      containerClasses.push('oneOfThree');
+    } else {
+      containerClasses.push('one');
+    }
+
+    if (this.topView) {
+      containerClasses.push('withTopRow');
+    }
+
+    const container = elementCreator.createContainer({ classes: containerClasses });
+
+    components.forEach((object) => {
+      const {
+        title,
+        component,
+      } = object;
+
+      if (title) {
+        container.appendChild(elementCreator.createSpan({
+          spanType: 'h2',
+          text: title,
+        }));
       }
 
-      if (topView) {
-        containerClasses.push('withTopRow');
-      }
-
-      const container = elementCreator.createContainer({ classes: containerClasses });
-
-      components.forEach((object) => {
-        const {
-          title,
-          component,
-        } = object;
-
-        if (title) {
-          container.appendChild(elementCreator.createSpan({
-            spanType: 'h2',
-            text: title,
-          }));
-        }
-
-        component.addToView({ element: container });
-      });
-
-      this.element.appendChild(container);
+      component.addToView({ element: container });
     });
+
+    return container;
+  }
+
+  replaceColumn({ column, replaceIndex }) {
+    let index = replaceIndex;
+
+    this.columns[index].components.forEach((object) => {
+      const { component } = object;
+
+      component.removeFromView();
+    });
+
+    if (this.topView) { index += 1; }
+
+    this.element.replaceChild(this.createColumn({ column }), this.element.childNodes[index]);
+  }
+
+  getTitle() {
+    return this.title || '-----';
   }
 }
 

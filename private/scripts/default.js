28c420706fbf678ae00c76694ca8453d6e215e2f
Aleksandar Jankovic <aleksonline@icloud.com>
2018-06-26 00:01:31 +0200

    Barebone default html/js/css

---
+++ private/scripts/default.js
@@ -0,0 +1,16 @@
+/*
+ Copyright 2018 Aleksandar Jankovic
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+const main = document.getElementById('main');
+
+main.appendChild(document.createElement('span').appendChild(document.createTextNode('Running default CSS/JS/HTML items. You can either change these files directly or change the paths in the Rolehaven config.')));

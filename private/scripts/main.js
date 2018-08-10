d8866d73112bf4d86dbae5f233c94d94d6b0cf43
Aleksandar Jankovic <aleksonline@icloud.com>
2018-08-10 02:22:57 +0200

    Updated interface

--- private/scripts/main.js
+++ private/scripts/main.js
@@ -5,7 +5,10 @@ const ViewWrapper = require('../library/components/ViewWrapper');
 const ChatView = require('../library/components/views/ChatView');
 const TopView = require('../library/components/views/StatusBar');
 const DocFileView = require('../library/components/views/DocFileView');
+const WalletView = require('../library/components/views/WalletView');
 
+const positionTracker = require('../library/PositionTracker');
+const viewTools = require('../library/ViewTools');
 const viewSwitcher = require('../library/ViewSwitcher').setParentElement({ element: document.getElementById('main') });
 
 const chatView = new ChatView({
@@ -14,6 +17,9 @@ const chatView = new ChatView({
 });
 const docFileView = new DocFileView({});
 const worldMapView = new WorldMapView({
+  alwaysShowLabels: {
+    line: true,
+  },
   maxZoom: 19,
   clusterStyle: {
     gridSize: 10,
@@ -24,11 +30,16 @@ const worldMapView = new WorldMapView({
       iconAnchor: [12, 12],
       textSize: 12,
       url: 'images/mapcluster.png',
-      textColor: '#000000',
+      textColor: '#ff00d7',
       fontFamily: 'monospace',
     }],
   },
-  backgroundColor: '#11433e',
+  labelStyle: {
+    fontColor: '#00ffef',
+    minZoomLevel: 19,
+    fontSize: 11,
+  },
+  backgroundColor: '#23001e',
   positionTypes: [
     'user',
     'device',
@@ -39,17 +50,37 @@ const worldMapView = new WorldMapView({
     'drivable-roads',
   ],
   polygonStyle: {
-    strokeColor: '#000000',
-    fillColor: '#0c9e00',
+    strokeColor: '#00ffef',
+    fillColor: '#ff00d7',
     opacity: 1,
     strokeOpacity: 1,
     fillOpacity: 1,
+    strokeWeight: 1.5,
   },
   markerStyle: {
     opacity: 0.9,
     icon: {
       url: '/images/mapicon-red.png',
     },
+  },
+  triggeredStyles: {
+    polygons: [{
+      paramName: 'description',
+      type: 'array',
+      minLength: 1,
+      style: {
+        strokeColor: '#ff00d7',
+        fillColor: '#00ffef',
+        styleName: 'Occupied',
+      },
+    }],
+  },
+  showLabelRules: {
+    polygons: [{
+      paramName: 'description',
+      type: 'array',
+      minLength: 1,
+    }],
   },
   choosableStyles: {
     markers: [{
@@ -64,23 +95,31 @@ const worldMapView = new WorldMapView({
       },
     }],
     polygons: [{
-      strokeColor: '#000000',
-      fillColor: '#9b0000',
+      strokeColor: '#ff0001',
+      fillColor: '#ff0001',
       styleName: 'Red',
     }, {
-      styleName: 'Green',
-      strokeColor: '#000000',
-      fillColor: '#00b402',
+      strokeColor: '#787878',
+      fillColor: '#787878',
+      styleName: 'Grey',
+    }, {
+      strokeColor: '#ff00d7',
+      fillColor: '#00ffef',
+      styleName: 'Occupied',
+    }, {
+      strokeColor: '#00ffef',
+      fillColor: '#ff00d7',
+      styleName: 'Not Occupied',
     }],
   },
   lineStyle: {
-    strokeColor: '#7f7f7f',
+    strokeColor: '#ffffff',
     strokeWeight: 2,
   },
   mapStyles: [{
     elementType: 'geometry',
     stylers: [
-      { color: '#11433e' },
+      { color: '#23001e' },
     ],
   }, {
     elementType: 'labels',
@@ -95,22 +134,17 @@ const worldMapView = new WorldMapView({
   }, {
     featureType: 'administrative',
     stylers: [
-      { color: '#000000' },
-    ],
-  }, {
-    featureType: 'landscape.man_made',
-    stylers: [
-      { color: '#11433e' },
+      { color: '#57004a' },
     ],
   }, {
     featureType: 'landscape.natural.terrain',
     stylers: [
-      { color: '#1a6962' },
+      { color: '#44003a' },
     ],
   }, {
     featureType: 'road',
     stylers: [
-      { color: '#696969' },
+      { color: '#555555' },
       { weight: 0.5 },
     ],
   }, {
@@ -121,14 +155,15 @@ const worldMapView = new WorldMapView({
   }, {
     featureType: 'water',
     stylers: [
-      { color: '#092522' },
+      { color: '#7d006c' },
     ],
   }],
 });
+const walletView = new WalletView({});
 
 const statusBar = new TopView({
   viewSwitcher,
-  title: 'SISCOM',
+  title: 'O3C',
   showClock: true,
   showControls: {
     user: true,
@@ -137,24 +172,13 @@ const statusBar = new TopView({
     room: true,
     view: true,
     docFile: true,
+    team: true,
   },
-});
-const mapWrapper = new ViewWrapper({
-  statusBar,
-  title: 'MAPS',
-  columns: [{
-    components: [
-      { component: worldMapView },
-    ],
-  }, {
-    components: [
-      { component: chatView },
-    ],
-  }],
 });
 const docWrapper = new ViewWrapper({
   statusBar,
-  title: 'DOCS',
+  viewType: viewSwitcher.ViewTypes.DOCS,
+  title: 'Docs',
   columns: [{
     components: [
       { component: docFileView },
@@ -167,7 +191,8 @@ const docWrapper = new ViewWrapper({
 });
 const chatWrapper = new ViewWrapper({
   statusBar,
-  title: 'COMS',
+  viewType: viewSwitcher.ViewTypes.CHAT,
+  title: 'Coms',
   columns: [{
     components: [
       { component: chatView },
@@ -180,9 +205,24 @@ const chatWrapper = new ViewWrapper({
 });
 const fullMapWrapper = new ViewWrapper({
   statusBar,
-  title: 'MAPS FOCUS',
+  viewType: viewSwitcher.ViewTypes.WORLDMAP,
+  title: 'Maps',
   columns: [{
     components: [{ component: worldMapView }],
+  }],
+});
+const walletWrapper = new ViewWrapper({
+  statusBar,
+  viewType: viewSwitcher.ViewTypes.WALLET,
+  title: 'Vcaps',
+  columns: [{
+    components: [
+      { component: walletView },
+    ],
+  }, {
+    components: [
+      { component: worldMapView },
+    ],
   }],
 });
 
@@ -190,10 +230,28 @@ statusBar.setViews({
   viewSwitcher,
   views: [
     { view: chatWrapper },
-    { view: mapWrapper },
     { view: docWrapper },
     { view: fullMapWrapper },
+    { view: walletWrapper },
   ],
 });
 
-viewSwitcher.switchView({ view: chatWrapper });
+document.addEventListener('click', () => {
+  viewTools.goFullScreen({});
+});
+
+viewSwitcher.addAvailableTypes({
+  types: [
+    chatWrapper.viewType,
+    fullMapWrapper.viewType,
+    walletWrapper.viewType,
+    docWrapper.viewType,
+  ],
+});
+viewSwitcher.setDefaultView({ view: chatWrapper });
+viewSwitcher.switchView({
+  setToDefault: true,
+  view: chatWrapper,
+});
+
+positionTracker.startTracker();

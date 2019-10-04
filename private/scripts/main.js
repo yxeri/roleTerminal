944a0feaeadc85985ed7e3a01eecb3b79a8b6579
Carmilla Mina Jankovic <carmillam@icloud.com>
2019-10-04 14:22:37 +0200

    Removed unused code

--- private/scripts/main.js
+++ private/scripts/main.js
@@ -169,30 +169,6 @@ if (deviceChecker.deviceType === deviceChecker.DeviceEnum.IOSOLD) {
   document.body.classList.add('oldIosFix');
 }
 
-const boot = new TextAnimation({
-  messages: systemLogo.concat([
-    { element: elementCreator.createSpan({ text: 'Connecting to HQ...' }) },
-    { element: elementCreator.createSpan({ text: '...' }) },
-    { element: elementCreator.createSpan({ text: '...' }), afterTimeout: 1000 },
-    { element: elementCreator.createSpan({ text: 'Failed to connect to HQ' }) },
-    { element: elementCreator.createSpan({ text: 'Rerouting...' }), afterTimeout: 1000 },
-    { element: elementCreator.createSpan({ text: 'Connected!' }) },
-    { element: elementCreator.createSpan({ text: 'Welcome to the Oracle, employee UNDEFINED.' }) },
-    { element: elementCreator.createSpan({ text: 'May you have a productive day!' }) },
-    { element: elementCreator.createSpan({ text: '' }) },
-    { element: elementCreator.createSpan({ text: 'Establishing uplink to relays...' }), afterTimeout: 1000 },
-    { element: elementCreator.createSpan({ text: 'Uplink established!' }) },
-    { element: elementCreator.createSpan({ text: 'Downloading modules...' }), afterTimeout: 1000 },
-    { element: elementCreator.createSpan({ text: 'LAMM  - LANTERN Amplification Master Manipulator' }) },
-    { element: elementCreator.createSpan({ text: 'RSAT  - O̶r̶g̶a̶n̶i̶c̶a RAZOR System Administrator Toolset' }) },
-    { element: elementCreator.createSpan({ text: 'CHAT  - Communication Host-Agent Tracker' }) },
-    { element: elementCreator.createSpan({ text: 'CREDS - Computer Registered Evaluative Decision System' }) },
-    { element: elementCreator.createSpan({ text: 'YOU   - YOU Object Unifier' }) },
-    { element: elementCreator.createSpan({ text: 'Booting O3S 7.1.3...' }), afterTimeout: 1000 },
-    { element: elementCreator.createSpan({ text: 'THIS RELEASE OF O3S WAS BROUGHT TO YOU BY' }) },
-  ]),
-});
-
 mouseHandler.allowRightClick = true;
 
 // boot.addToView({ element: viewSwitcher.getParentElement() });
